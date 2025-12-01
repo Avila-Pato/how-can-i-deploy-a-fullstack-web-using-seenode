8b62c458c67fd3a4e26783162b9f1db64a290167
Avila-Pato <p.avilaf1998@gmail.com>
2025-12-01 12:03:18 -0300

    installig prisma to have a db

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 
   images: {
-    domains: ["m.media-amazon.com"],
+    domains: ["m.media-amazon.com", "upload.wikimedia.org", "i0.wp.com"],
   },
   /* config options here */
   reactCompiler: true,
--- server/index.js
+++ server/index.js
@@ -1,20 +1,22 @@
 import express from "express"
+import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
+import pg from "pg"
+import dotenv from "dotenv"
 
+dotenv.config()
 
 const app = express()
 
-const port  = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
-app.get("/products", (req, res) => {
-    res.json([
-        {
-            id: "1",
-            name: "Indian Man",
-            price: 100,
-            description: "Indian Man",
-            imageUrl: "https://m.media-amazon.com/images/I/71+IREvx7YL._AC_SL1200_.jpg",
-        }
-    ])
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
+
+app.get("/products", async (req, res) => {
+    const products = await prisma.product.findMany()
+    return res.json(products)
 })
 
 app.listen(port, () => {
